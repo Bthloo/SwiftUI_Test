0e1ac62e58ad2aa8a6bb6122f0eece7d36937557
bthloo <basselalaa55@gmail.com>
2024-10-03 23:23:22 +0300

    Initial Commit

---
+++ SwiftUI_Test/SwiftUI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TestApp.swift
+//  SwiftUI_Test
+//
+//  Created by Bthloo on 03/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
